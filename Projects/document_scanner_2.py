ff74359c7ca386349c7718b417064d278554eca1
Abhivridh <abhivridh2@gmail.com>
2025-12-19 22:54:03 +0530

    next commit

--- Projects/document_scanner_2.py
+++ Projects/document_scanner_2.py
@@ -12,7 +12,7 @@ def resize_image(frame, scale=0.5):
     return cv.resize(frame, d, interpolation=scale_mode)
 
 
-img = cv.imread('../Images/Back-Side-Mehndi-.jpg')
+img = cv.imread('../Images/photo_6194762285033655125_y.jpg')
 cv.imshow('Original Image', img)
 
 resized_img = resize_image(img, 0.5)
@@ -27,8 +27,25 @@ cv.imshow('Blurred Image', blur)
 # edges = cv.Canny(resized_img, 100, 200)
 # cv.imshow('Canny Edges', edges)
 
-ret, thresh = cv.threshold(gray, 125,255,cv.THRESH_BINARY)
+ret, thresh = cv.threshold(blur, 0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
 cv.imshow('Thresholded Image', thresh)
+
+contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key=cv.contourArea, reverse=True)
+print(f'{len(contours)} contour(s) found')
+
+max_area = 0;
+for contour in contours:
+    area = cv.contourArea(contour)
+    if area > 1000:
+        peri = cv.arcLength(contour, True)
+        approx = cv.approxPolyDP(contour, 0.015 * peri, True)
+        if area > max_area and len(approx) == 4:
+            document_contour  = approx
+            max_area = area
+
+cv.drawContours(resized_img, [document_contour], -1, (0,255,0), 3)
+cv.imshow('Biggest Contour', resized_img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
